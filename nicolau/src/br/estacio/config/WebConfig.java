b16b11f6c522237726cbb374d2be13e457b9c495
nicolauocabral <31347744+nicolauocabral@users.noreply.github.com>
2017-08-25 10:09:13 -0300

    Add files via upload

---
+++ nicolau/src/br/estacio/config/WebConfig.java
@@ -0,0 +1,13 @@
+@EnableWebMvc
+@Configuration
+@ComponentScan({ "br.estacio" })
+
+package br.estacio.config;
+
+
+import WebMvcConfigurerAdapter;
+
+
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
